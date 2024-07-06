922f3f76eba3e78fcef4613fd37d541d05a65732
khouloud Haddad Amamou <khouloud.haddad@gmail.com>
2024-07-06 21:36:34 +0100

    Register form

--- app/patients/[userId]/register/page.tsx
+++ app/patients/[userId]/register/page.tsx
@@ -1,8 +1,11 @@
-import PatientForm from "@/components/forms/PatientForm";
-import { Link } from "lucide-react";
 import Image from "next/image";
+import { Link } from "lucide-react";
+import RegisterForm from "@/components/forms/RegisterForm";
+import { getPatient, getUser } from "@/lib/actions/patient.actions";
 
-export default function Home() {
+const Register = async ({ params: { userId } }: SearchParamProps) => {
+  const user = await getUser(userId);
+  const patient = await getPatient(userId);
   return (
     <div className="flex h-screen max-h-screen">
       <section className="remove-scrollbar container my-auto">
@@ -15,7 +18,7 @@ export default function Home() {
             className="mb-12 h-10 w-fit"
           />
 
-          <PatientForm />
+          <RegisterForm user={user} />
 
           <div className="text-14-regular mt-20 flex justify-between">
             <p className="justify-items-end text-dark-600 xl:text-left">
@@ -25,16 +28,18 @@ export default function Home() {
               Admin
             </Link>
           </div>
-          
+
         </div>
       </section>
       <Image
-        src="/assets/images/onboarding-img.png"
+        src="/assets/images/register-img.png"
         height={1000}
         width={1000}
         alt="patient"
-        className="side-img max-w-[50%]"
+        className="side-img max-w-[390px]"
       />
     </div>
   )
 }
+
+export default Register
